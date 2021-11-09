55bbb852f53fdd2e8fa9bd3888f5e6a9ad4d45a5
paria340 <paria340@gmail.com>
2021-11-08 18:40:20 -0800

    modified

--- scripts.js
+++ scripts.js
@@ -21,14 +21,13 @@ const movieApp = {}
 movieApp.apiKey = `bfb23e9c017a2be83f91472023334cb6`
 movieApp.apiUrl = 'https://api.themoviedb.org/3/search/movie'
 
-movieApp.userSearchTerm = document.querySelector('input[name=search]').value;
-movieApp.userLanguage = document.querySelector('select[name=language]').value;
 
-movieApp.getMovieInfo = () => {
+movieApp.getMovieInfo = (argument) => {
     const url = new URL(movieApp.apiUrl)
     url.search = new URLSearchParams({
         api_key: movieApp.apiKey,
-        query: movieApp.userSearchTerm,
+        query: argument,
+        page: 1,
         //language: movieApp.userLanguage
     })
     fetch(url)
@@ -36,9 +35,16 @@ movieApp.getMovieInfo = () => {
             return response.json()
         })
         .then((jsonResponse) => {
-            console.log(jsonResponse)
             movieApp.displayMovieInfo(jsonResponse)
+            //movieApp.getLanguage(jsonResponse)
+            //movieApp.fiveRandomOptions(jsonResponse)
         })
+}
+
+movieApp.getLanguage = (dataLanguage) => {
+    let currentLanguage = dataLanguage.filter((item) => {
+        return movieApp.userLanguage === dataLanguage.original_language
+    })
 }
 
 movieApp.expandingBar = () => {
@@ -54,57 +60,66 @@ movieApp.expandingBar = () => {
 }
 
 movieApp.displayMovieInfo = (dataMovie) => {
+    
+    const firstFive = dataMovie.results.slice(0, 4)
+    console.log(firstFive)
 
     const ulElement = document.querySelector('ul')
-    const formElement = document.querySelector('form')
-    // const summary = document.querySelector('.overview')
-
-    formElement.addEventListener('submit', function(e){
-        e.preventDefault();
-        dataMovie.results.forEach((item) => {
-            const li = document.createElement('li')
-            const img = document.createElement('img')
-            const infoElement = document.createElement('p')
-            // const titleElement = document.createElement('p')
-            // const popularityElement = document.createElement('p')
-            const imgDefault = 'https://image.tmdb.org/t/p/w500'
-
-            img.src = imgDefault.concat(item.poster_path)
-            img.alt = item.title
-
-            li.append(img)
-            ulElement.appendChild(li)
-
-            infoElement.innerHTML = `<div>${item.original_title}</div><p>${item.overview}</p><div>${item.popularity}</div>`
-            li.append(infoElement)
-
-            // infoElement.textContent = item.overview
-            // li.append(infoElement)
-
-            // titleElement.textContent = item.original_title
-            // li.append(titleElement)
-
-            // popularityElement.textContent = item.popularity
-            // li.append(popularityElement)
-
-            //adding
-            //formElement.style.display = 'none'
-
-
+    firstFive.forEach((item) => {
+            if(item.poster_path && item.overview){
+                const li = document.createElement('li')
+                const img = document.createElement('img')
+                const infoElement = document.createElement('p')
+                const imgDefault = 'https://image.tmdb.org/t/p/w500'
+    
+                img.src = imgDefault.concat(item.poster_path)
+                img.alt = item.title
+    
+                li.append(img)
+                ulElement.appendChild(li)
+    
+                infoElement.innerHTML = `<div>${item.original_title}</div><p>${item.overview}</p><div>${item.popularity}</div>`
+                li.append(infoElement)
+            }
+    
         });
-    })
 }  
 
+movieApp.emptyResults = () => {
+    const resultsEl = document.querySelector('.results')
+    resultsEl.innerHTML = ""
+}
 
 movieApp.init = () => {
     movieApp.expandingBar()
-    movieApp.getMovieInfo()
+    // const ulElement = document.querySelector('ul')
+    const formElement = document.querySelector('form')
+    formElement.addEventListener('submit', function(e){
+        e.preventDefault();
+        const headerElement = document.querySelector('header')
+        const iconElement = document.querySelector('i')
+        headerElement.style.display = 'none' 
+        iconElement.style.display = 'block'
+        movieApp.userSearchTerm = document.querySelector('input[name=search]').value;
+        //movieApp.userLanguage = document.querySelector('select[name=language]').value;
+        movieApp.getMovieInfo(movieApp.userSearchTerm)
+    })
 }
 
 movieApp.init()
 
 
-
 // const resultsEl = document.querySelector('.results')
 // resultsEl.innerHTML = ""
 //movieApp.userSearchTerm.textContent = ''
+
+// if(movieApp.userSearchTerm !== ""){
+//     movieApp.getMovieInfo()
+// }else{
+//     alert("enter a title!")
+// }
+
+
+// const choices = []
+// choices = movieApp.fiveRandomOptions()
+// console.log(choices)
